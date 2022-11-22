971edfc4870a162e05e6a293f976797f8ec71b49
nikita <nikita.raevnev@gmail.com>
2022-11-22 21:50:45 +0300

    auth

---
+++ project/src/pages/main-page-no-auth/main-page-no-auth.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import { AppRouters } from '../../router/app-routers';
+
+export function MainPageNoAuth(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRouters.LOGIN}>
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__login">Sign in</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+
+        </div>
+      </main>
+    </div>
+  );
+}
